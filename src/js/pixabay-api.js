3e54b593a26dded7e95e121f13c47f3df0e40b14
VitaliyKharytonov <vitalicaxaritonov@gmail.com>
2024-03-23 23:32:30 +0200

    add task and css

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+export function getImage(value) {
+  const KEY = '22926721-5d20aa08498ffd1ff2f906542';
+  const BASE_URL = 'https://pixabay.com/api/';
+
+  const params = new URLSearchParams({
+    key: KEY,
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `${BASE_URL}?${params}`;
+
+  return fetch(url).then(res => res.json());
+}
